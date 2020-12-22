520f91b26565bc16e74963c847382c7a558e6a00
Zach Chan <zacharyleechan@gmail.com>
2020-12-22 09:29:25 -0800

    replaced button text with square and added shadow

--- components/record-button.js
+++ components/record-button.js
@@ -6,27 +6,38 @@ const RecordButton = () => {
     <TouchableOpacity
       onPress={() => alert('Recording')}
       style={styles.button}>
-      <Text style={styles.buttonText}>
-        Start Activity
-      </Text>
+      <View style={styles.buttonSquare}></View>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: '#eb4634',
+    backgroundColor: '#fff',
+    borderStyle: 'solid',
+    borderWidth: 5,
+    borderColor: '#eb4634',
     height: 110,
     width: 110,
     padding: 20,
     borderRadius: 110,
     marginTop: 100,
     justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#888',
+    shadowRadius: 2,
+    shadowOpacity: 1,
+    shadowOffset: { width: 0, height: 0.5 },
   },
-  buttonText: {
-    fontSize: 20,
-    color: '#fff',
-    textAlign: 'center',
+  buttonSquare: {
+    backgroundColor: '#eb4634',
+    width: 40,
+    height: 40,
+    borderRadius: 5,
+    shadowColor: '#888',
+    shadowRadius: 2,
+    shadowOpacity: 1,
+    shadowOffset: { width: 0, height: 0.5 },
   },
 });
 
